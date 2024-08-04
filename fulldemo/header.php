2a8a73e3cf34ea159b7a5797e57781074af013a8
josspheenboles <eng.jospheenboles@gmail.com>
2024-08-04 22:40:42 +0300

    template

---
+++ fulldemo/header.php
@@ -0,0 +1,16 @@
+<table width=100% height=100% border=1px solid>
+    <tr>
+        <td colspan=9>
+            <h1>Header</h1>
+            
+        </td>
+    </tr>    
+
+    <tr>
+        <td colspan=2>
+            <a href="Home.php">Home</a></br>
+            <a href="List.php">Trainees</a></br>
+            
+        </td>
+    
+    
